062af54a4db60cb0c5e722cea42dbf65c37fb371
gyt-Macmini <igyteng@gmail.com>
2016-05-04 00:56:14 +0800

    login

--- public/controllers/login.js
+++ public/controllers/login.js
@@ -1,36 +1,4 @@
-var app = angular.module('LoginApp', ['ngMaterial', 'ui.router', 'ngMessages']);
 
-app.config(
-    ['$stateProvider', '$urlRouterProvider',
-        function($stateProvider, $urlRouterProvider) {
-
-            $urlRouterProvider
-                .when('/', '/index/login')
-                .otherwise('/index/login');
-
-            $stateProvider
-                .state('index', {
-                    abstract: true,
-                    url: '/index',
-                    templateUrl: '/public/views/index.html',
-                })
-
-                .state('index.login', {
-                    url: '/login',
-                    controller: 'LoginController',
-                    templateUrl: '/public/views/login.html'
-                })
-                // .state('index.tab2', {
-                //     url: '/tab2',
-                //     controller: function($scope) {
-
-                //     },
-                //     templateUrl: '/public/views/tab2.html'
-                // })
-            ;
-        }
-    ]
-);
 
 app.controller('LoginController', function($scope, $http) {
     $scope.user = {
--- server/routes/login.js
+++ server/routes/login.js
@@ -1,7 +1,12 @@
 var app = global.app;
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
     res.render('login', {
-
+        'controllers': [
+            '/public/controllers/login.js'
+        ],
+        'routes': [
+            '/public/routes/login.js'
+        ]
     });
 });
