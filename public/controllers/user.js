c81df83d28dc81131b6f442daf18e4df78522225
gyt-Macmini <igyteng@gmail.com>
2016-05-07 00:10:20 +0800

    侧边栏

--- public/controllers/user.js
+++ public/controllers/user.js
@@ -3,7 +3,13 @@ app.controller('UserIndexController', function($scope, $http, $state) {
         $http.post('/user/logout');
     };
 })
-.controller('TestCtrl', function($scope, $http, $state) {
-    $scope.messages = [{what: '1awf', who: 'zgerf', notes: 'zgwergew'}];
+.controller('TestCtrl', function($scope, $http, $state, $mdSidenav) {
+    $scope.showMenu = function() {
+        $mdSidenav('left').toggle();
+    };
+    $scope.settings = [
+    { name: 'Wi-Fi', extraScreen: 'Wi-fi menu', icon: 'wifi', enabled: true },
+    { name: 'Bluetooth', extraScreen: 'Bluetooth menu', icon: 'bluetooth', enabled: false },
+  ];
 })
 ;
