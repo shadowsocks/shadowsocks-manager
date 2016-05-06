627ed199b2ec87e5c7d515d44dfbf42a5edea0d9
gyt-Macmini <igyteng@gmail.com>
2016-05-06 23:10:37 +0800

    sidebar 

--- public/controllers/user.js
+++ public/controllers/user.js
@@ -2,4 +2,8 @@ app.controller('UserIndexController', function($scope, $http, $state) {
     $scope.logout = function() {
         $http.post('/user/logout');
     };
-});
+})
+.controller('TestCtrl', function($scope, $http, $state) {
+    $scope.messages = [{what: '1awf', who: 'zgerf', notes: 'zgwergew'}];
+})
+;
