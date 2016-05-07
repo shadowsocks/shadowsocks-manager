e3df62fb3e6bc53c0cc1ef1f3b4949548420a3e2
gyt-Macmini <igyteng@gmail.com>
2016-05-07 11:28:37 +0800

    add fab button

--- public/controllers/admin.js
+++ public/controllers/admin.js
@@ -3,15 +3,21 @@ app.controller('AdminMainController', function($scope, $http, $state, $mdSidenav
             $mdSidenav('left').toggle();
         };
         $scope.menus = [
-            {name: '服务器管理', icon: 'cloud', click: function() {
-                $state.go('admin.server');
-                $mdSidenav('left').close();
-            }},
+            {name: '首页', icon: 'home', click: 'admin.index'},
+            {name: '服务器管理', icon: 'cloud', click: 'admin.server'},
             {name: '用户管理', icon: 'face'},
             {name: '续费码', icon: 'shop'},
             {name: '流量统计', icon: 'timeline'},
             {name: '历史记录', icon: 'watch_later'}
         ];
+        $scope.publicInfo = {
+            currentMenu: 0
+        };
+        $scope.menuClick = function(index) {
+            $scope.publicInfo.currentMenu = index;
+            $state.go($scope.menus[index].click);
+            $mdSidenav('left').close();
+        };
         $scope.bottomMenus = [
             {name: '退出登录', icon: 'exit_to_app', click: function() {
                 $http.post('/user/logout').success(function(data) {
