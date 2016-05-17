be2bdd745460e3eb498093e2b713af65f0c9a86a
Gyt <igyteng@gmail.com>
2016-05-17 11:13:58 +0800

    增加账号状态

--- server/models/server.js
+++ server/models/server.js
@@ -5,11 +5,18 @@ var serverSchema = new Schema({
     name: {type:String, required:true, unique:true},
     ip: String,
     port: Number,
-    account: [{
-        port: Number,
+    account: [new Schema({
+        port: {type:Number, required:true, unique:true},
         password: String,
-        expireTime: Date
-    }]
+        expireTime: {type: Date, default: new Date()},
+        flow: {type: Number, default: 0},
+        status: {type:Number, default: 0}
+        /*
+        status定义: 0 根据expireTime和flow自动开启和关闭
+                    1 关闭
+                    2 常开
+        */
+    }, { _id: false })]
 });
 
 serverSchema.index({ip: 1, port: 1}, {unique: true});
