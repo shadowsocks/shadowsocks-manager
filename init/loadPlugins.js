f4074be15e7d95265d0194d986cbd5f156be4309
gyt-Macmini <igyteng@gmail.com>
2016-10-09 00:24:20 +0800

    load Plugins

--- init/loadPlugins.js
+++ init/loadPlugins.js
@@ -1,17 +1,14 @@
 const path = require('path');
-module.exports = function (ctx, next) {
-  const config = ctx.config.all();
-  if(!config.plugins) {
-    return;
+const config = appRequire('services/config').all();
+if(!config.plugins) {
+  return;
+}
+if(config.type !== 'm') {
+  return;
+}
+for(const name in config.plugins) {
+  if(config.plugins[name].use) {
+    console.log(`Load plugin: ${ name }`);
+    appRequire(`plugins/${ name }/index`);
   }
-  if(config.type !== 'm') {
-    return;
-  }
-  for(const name in config.plugins) {
-    if(config.plugins[name].use) {
-      console.log(`Load plugin: ${ name }`);
-      ctx.task(path.resolve(__dirname, `../plugins/${ name }/index.js`));
-    }
-  }
-  next();
-};
+}
