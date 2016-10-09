289881d5a947f9147d616e7f5978344b54260a68
Guyuteng <igyteng@gmail.com>
2016-10-09 10:12:30 +0800

    add config.js

---
+++ services/config.js
@@ -0,0 +1,28 @@
+const yaml = require('js-yaml');
+const fs   = require('fs');
+const os   = require('os');
+const path = require('path');
+const _ = require('lodash');
+
+let configFile;
+
+try {
+  configFile = yaml.safeLoad(fs.readFileSync(path.resolve(os.homedir() + '/.ssmgr/default.yml'), 'utf8'));
+} catch (e) {
+  console.log(e);
+}
+
+exports.all = () => {
+  return configFile;
+};
+
+exports.get = (path) => {
+  if(!configFile) {
+    return;
+  }
+  return _.get(configFile, path);
+};
+
+exports.set = (path, value) => {
+  return _.set(configFile, path, value);
+};
