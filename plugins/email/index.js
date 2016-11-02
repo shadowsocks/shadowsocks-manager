a865df2780426a1de82cda29d9169df3d10ac81a
Guyuteng <igyteng@gmail.com>
2016-11-02 12:09:43 +0800

    checkcode

--- plugins/email/index.js
+++ plugins/email/index.js
@@ -24,24 +24,55 @@ const sendMail = async (options) => {
 };
 
 const sendCode = async (to) => {
-  const code = Math.random().toString().substr(2, 6);
-  await transporter.sendMail({
-    from: config.plugins.email.username,
+  const sendEmailTime = 10;
+  try {
+    const findEmail = await knex('email').select(['remark']).where({
+      to,
+      type: 'code',
+    }).whereNotBetween('time', [Date.now() - sendEmailTime * 60 * 1000, Date.now()]);
+    if(findEmail.length > 0) {
+      return findEmail[0].remark;
+    }
+    const code = Math.random().toString().substr(2, 6);
+    await transporter.sendMail({
+      from: config.plugins.email.username,
+        to,
+        subject: 'Hello',
+        text: 'Your code is [' + code + ']',
+    });
+    await knex('email').insert({
       to,
       subject: 'Hello',
       text: 'Your code is [' + code + ']',
-  });
-  await knex('email').insert({
-    to,
-    subject: 'Hello',
-    text: 'Your code is [' + code + ']',
-    type: 'code',
-    remark: code,
-    time: Date.now(),
-  });
-  return;
+      type: 'code',
+      remark: code,
+      time: Date.now(),
+    });
+    return code;
+  } catch (err) {
+    return Promise.reject(err);
+  }
 };
 
+const checkCode = async (email, code) => {
+  const sendEmailTime = 10;
+  try {
+    const findEmail = await knex('email').select(['remark']).where({
+      to: email,
+      remark: code,
+      type: 'code',
+    }).whereNotBetween('time', [Date.now() - sendEmailTime * 60 * 1000, Date.now()]);
+    console.log(findEmail);
+    if(findEmail.length === 0) {
+      return Promise.reject();
+    }
+  } catch(err) {
+    return Promise.reject(err);
+  }
+};
+
+
+// checkCode('igyteng@gmail.com', '505740').then();
 // sendCode('igyteng@gmail.com').then(console.log);
 // sendMail({
 //   from: config.plugins.email.username,
