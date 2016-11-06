3b94cf89c63a9ee922ab5b0f5292794240ad2643
gyt-Macmini <igyteng@gmail.com>
2016-11-06 17:02:51 +0800

    check code

--- plugins/freeAccount/server/app.js
+++ plugins/freeAccount/server/app.js
@@ -8,7 +8,9 @@ app.post('/email', (req, res) => {
   email.sendCode(emailAddress, 'Hello', 'Your code is:').then(s => res.send(s), e => res.send(e));
 });
 app.post('/code', (req, res) => {
-
+  const emailAddress = req.body.email;
+  const code = req.body.code;
+  email.checkCode(emailAddress, code).then(s => res.send(s), e => res.send(e));
 });
 
 app.get('/', (req, res) => {
