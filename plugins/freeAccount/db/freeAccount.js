4a346cfd5fe288375fa2e11e2b4c7d47208973ab
Guyuteng <igyteng@gmail.com>
2016-11-08 10:14:49 +0800

    free account db

---
+++ plugins/freeAccount/db/freeAccount.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const knex = appRequire('init/knex').knex;
+const tableName = 'freeAccount';
+
+const config = appRequire('services/config').all();
+const createTable = async() => {
+  if(config.empty) {
+    await knex.schema.dropTableIfExists(tableName);
+  }
+  return knex.schema.createTableIfNotExists(tableName, function(table) {
+    table.string('address').primary();
+    table.string('email');
+    table.integer('port');
+    table.integer('flow');
+    table.dateTime('time');
+    table.dateTime('expired');
+    table.boolean('isDisabled');
+  });
+};
+
+exports.createTable = createTable;
--- plugins/freeAccount/server/app.js
+++ plugins/freeAccount/server/app.js
@@ -1,8 +1,30 @@
 'use strict';
 
 const app = appRequire('plugins/freeAccount/index').app;
+const knex = appRequire('init/knex').knex;
+const manager = appRequire('services/manager');
 const email = appRequire('plugins/email/index');
 const path = require('path');
+
+const randomAccount = async (email, flow) => {
+  const port = 56342;
+  const password = '12345';
+  try {
+    await manager.send({
+      command: 'add',
+      port,
+      password,
+    });
+    const address = 'abcdefg';
+    await knex('freeAccount').insert({
+      address, email, port, flow, time: Date.now(), expired: Date.now() + 5 * 60 * 1000, isDisabled: false,
+    });
+    return;
+  } catch(err) {
+    console.log(err);
+    return Promise.reject(err);
+  }
+};
 
 app.post('/email', (req, res) => {
   req.checkBody('email', 'Email address error').notEmpty().isEmail();
@@ -14,11 +36,18 @@ app.post('/email', (req, res) => {
   email.sendCode(emailAddress, 'Hello', 'Your code is:')
   .then(s => res.send('success'), e => res.status(403).end());
 });
+
 app.post('/code', (req, res) => {
   const emailAddress = req.body.email;
   const code = req.body.code;
   email.checkCode(emailAddress, code)
-  .then(s => res.send('success'), e => res.status(403).end());
+  .then(success => {
+    return randomAccount(emailAddress, 100000);
+  }).then(success => {
+    res.send('success');
+  }).catch(error => {
+    res.status(403).end();
+  });
 });
 
 app.get('/', (req, res) => {
