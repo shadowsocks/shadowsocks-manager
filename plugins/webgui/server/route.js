818445fd796b31fa70c669b8d5aff93784026b3d
Guyuteng <igyteng@gmail.com>
2016-12-20 16:28:46 +0800

    null

--- plugins/webgui/server/route.js
+++ plugins/webgui/server/route.js
@@ -1,9 +1,11 @@
 'use strict';
 
 const app = appRequire('plugins/webgui/index').app;
+const user = appRequire('plugins/webgui/server/user');
+const admin = appRequire('plugins/webgui/server/admin');
 
 app.post('/api/email/code');
-app.post('/api/user/signup');
+app.post('/api/user/signup', user.signup);
 app.post('/api/user/login');
 app.post('/api/user/logout');
 
--- plugins/webgui/server/user.js
+++ plugins/webgui/server/user.js
@@ -1,1 +1,7 @@
 'use strict';
+
+const user = appRequire('plugins/user/index');
+
+exports.signup = (req, res) => {
+  user.addUser();
+};
