b0e616e54a5067df76eaab91b2eec6b09a5a635d
Guyuteng <igyteng@gmail.com>
2016-12-22 16:14:37 +0800

    null

--- plugins/webgui/server/admin.js
+++ plugins/webgui/server/admin.js
@@ -1,1 +1,35 @@
 'use strict';
+
+const manager = appRequire('services/manager');
+const knex = appRequire('init/knex').knex;
+
+exports.getServers = (req, res) => {
+  knex('server').select().then(success => {
+    res.send(success);
+  }).catch(err => {
+    res.status(500).end();
+  });
+};
+
+exports.getOneServer = (req, res) => {
+  const serverName = req.params.serverName;
+  let result = null;
+  knex('server').select().where({
+    name: serverName,
+  }).then(success => {
+    if(success.length) {
+      result = success[0];
+      return manager.send({
+        command: 'list',
+        port: success[0].port,
+        password: success[0].password,
+      });
+    }
+    res.status(404).end();
+  }).then(success => {
+    result.ports = success;
+    res.send(result);
+  }).catch(err => {
+    res.status(500).end();
+  });
+};
--- plugins/webgui/server/route.js
+++ plugins/webgui/server/route.js
@@ -5,13 +5,22 @@ const user = appRequire('plugins/webgui/server/user');
 const admin = appRequire('plugins/webgui/server/admin');
 
 const isUser = () => {};
-const isAdmin = () => {};
+const isAdmin = (req, res, next) => {
+  if(req.session.type === 'admin') {
+    return next();
+  } else {
+    return res.status(401).end();
+  }
+};
 
 app.get('/api/login', user.status);
 app.post('/api/email/code');
 app.post('/api/signup', user.signup);
 app.post('/api/login', user.login);
 app.post('/api/logout', user.logout);
+
+app.get('/api/admin/server', isAdmin, admin.getServers);
+app.get('/api/admin/server/:serverName', isAdmin, admin.getOneServer);
 
 app.get('*', (req, res) => {
   return res.render('index');
