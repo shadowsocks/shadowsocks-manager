b7c72e8e8f72d7e566cd094f950d1d6d0827eec6
Guyuteng <igyteng@gmail.com>
2017-02-07 11:55:24 +0800

    add account time limit after pay

--- plugins/alipay/index.js
+++ plugins/alipay/index.js
@@ -4,7 +4,7 @@ const knex = appRequire('init/knex').knex;
 
 const createOrder = async (user, account, amount) => {
   const orderId = Math.random().toString().substr(2);
-  const time = 5;
+  const time = 30;
   const qrCode = await alipay_f2f.createQRPay({
     tradeNo: orderId,
     subject: 'ss',
@@ -29,9 +29,11 @@ const createOrder = async (user, account, amount) => {
 };
 
 setInterval(async () => {
-  const orders = await knex('alipay').select().whereNotBetween('expireTime', [0, Date.now()]);
+  const orders = await knex('alipay').select();
+  // .whereNotBetween('expireTime', [0, Date.now()])
   orders.forEach(order => {
-    if(order.status !== 'TRADE_SUCCESS') {
+
+    if(order.status !== 'TRADE_SUCCESS' && order.status !== 'FINISH') {
       alipay_f2f.checkInvoiceStatus(order.orderId).then(success => {
         if(success.code === '10000') {
           knex('alipay').update({
@@ -41,9 +43,29 @@ setInterval(async () => {
           }).then();
         }
       });
+    } else if(order.status === 'TRADE_SUCCESS') {
+      const accountId = order.account;
+      knex('account_plugin').select().where({id: accountId}).then(success => {
+        if(success.length) {
+          const accountData = JSON.parse(success[0].data);
+          accountData.limit += 1;
+          return accountData;
+        }
+        return Promise.reject();
+      }).then(success => {
+        return knex('account_plugin').update({
+          data: JSON.stringify(success),
+        }).where({id: accountId});
+      }).then(() => {
+        return knex('alipay').update({
+          status: 'FINISH',
+        }).where({
+          orderId: order.orderId,
+        });
+      }).then().catch(console.log);
     };
   });
-}, 30 * 1000);
+}, 10 * 1000);
 
 const checkOrder = async (orderId) => {
   const order = await knex('alipay').select().where({
