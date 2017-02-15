80491fa87e619c0e34e2c1b30577dc5d1b08577d
gyt-Macmini <igyteng@gmail.com>
2017-02-15 22:14:07 +0800

    order page

--- plugins/webgui/public/services/adminService.js
+++ plugins/webgui/public/services/adminService.js
@@ -4,7 +4,11 @@ app.factory('adminApi', ['$http', $http => {
   const getUser = () => {
     return $http.get('/api/admin/user').then(success => success.data);
   };
+  const getOrder = () => {
+    return $http.get('/api/admin/order').then(success => success.data);
+  };
   return {
     getUser,
+    getOrder,
   };
 }]);
