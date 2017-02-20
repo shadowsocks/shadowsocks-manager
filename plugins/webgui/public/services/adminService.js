d66b230dc0cd6ac529a965fc0b6fe2fdd3dd41af
gyt-Macmini <igyteng@gmail.com>
2017-02-21 01:06:31 +0800

    localStorage server info

--- plugins/webgui/public/services/adminService.js
+++ plugins/webgui/public/services/adminService.js
@@ -7,6 +7,43 @@ app.factory('adminApi', ['$http', '$q', ($http, $q) => {
   const getOrder = () => {
     return $http.get('/api/admin/order').then(success => success.data);
   };
+  const getServer = () => {
+    return $http.get('/api/admin/server').then(success => success.data);
+  };
+  const getServerFlow = serverId => {
+    return $q.all([
+      $http.get('/api/admin/flow/' + serverId, {
+        params: {
+          time: [
+            moment().hour(0).minute(0).second(0).millisecond(0).toDate().valueOf(),
+            moment().toDate().valueOf(),
+          ],
+        }
+      }),
+      $http.get('/api/admin/flow/' + serverId, {
+        params: {
+          time: [
+            moment().day(0).hour(0).minute(0).second(0).millisecond(0).toDate().valueOf(),
+            moment().toDate().valueOf(),
+          ],
+        }
+      }),
+      $http.get('/api/admin/flow/' + serverId, {
+        params: {
+          time: [
+            moment().date(1).hour(0).minute(0).second(0).millisecond(0).toDate().valueOf(),
+            moment().toDate().valueOf(),
+          ],
+        }
+      }),
+    ]).then(success => {
+      return {
+        today: success[0].data[0],
+        week: success[1].data[0],
+        month: success[2].data[0],
+      };
+    });
+  };
   const getAccountId = port => {
     return $http.get('/api/admin/account/port/' + port).then(success => success.data.id);
   };
@@ -36,6 +73,8 @@ app.factory('adminApi', ['$http', '$q', ($http, $q) => {
   return {
     getUser,
     getOrder,
+    getServer,
+    getServerFlow,
     getAccountId,
     getIndexInfo,
     getServerPortData,
