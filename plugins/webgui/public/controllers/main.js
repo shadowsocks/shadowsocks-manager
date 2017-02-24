8356ea8fdd9daf87b348b52ec66549f458256e38
gyt-Macmini <igyteng@gmail.com>
2017-02-25 00:50:17 +0800

    safari

--- plugins/webgui/public/controllers/main.js
+++ plugins/webgui/public/controllers/main.js
@@ -1,7 +1,7 @@
 const app = require('../index').app;
 
-app.controller('MainController', ['$scope', '$localStorage',
-  ($scope, $localStorage) => {
+app.controller('MainController', ['$scope', '$localStorage', '$location',
+  ($scope, $localStorage, $location) => {
     $scope.version = window.ssmgrVersion;
     $localStorage.$default({
       admin: {},
@@ -14,6 +14,20 @@ app.controller('MainController', ['$scope', '$localStorage',
     };
     document.addEventListener('visibilitychange', () => {
       $scope.$broadcast('visibilitychange', document.visibilityState);
+    });
+    const isSafari = () => {
+      const ua = navigator.userAgent;
+      const iOS = !!ua.match(/iPad/i) || !!ua.match(/iPhone/i);
+      const webkit = !!ua.match(/WebKit/i);
+      const standalone = !!window.navigator.standalone;
+      const iOSSafari = iOS && webkit && !ua.match(/CriOS/i);
+      return iOSSafari && standalone;
+    };
+    if(isSafari() && $location.url() === '/home/index' && $localStorage.home.url !== '/home/index') {
+      location.href = $localStorage.home.url;
+    }
+    $scope.$on('$stateChangeSuccess', () => {
+      $localStorage.home.url = $location.url();
     });
   }
 ]);
