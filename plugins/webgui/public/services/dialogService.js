610f49f581f900533177c95c93cdba6708c88521
gyt-Macmini <igyteng@gmail.com>
2017-02-25 13:15:05 +0800

    change pwd dialog

--- plugins/webgui/public/services/dialogService.js
+++ plugins/webgui/public/services/dialogService.js
@@ -249,3 +249,72 @@ app.factory('orderDialog', [ '$mdDialog', ($mdDialog) => {
     show,
   };
 }]);
+
+app.factory('changePasswordDialog', [ '$mdDialog', 'userApi', ($mdDialog, userApi) => {
+  const publicInfo = {
+    status: 'show',
+  };
+  let dialogPromise = null;
+  const isDialogShow = () => {
+    if(dialogPromise && !dialogPromise.$$state.status) {
+      return true;
+    }
+    return false;
+  };
+  const show = (accountId, password) => {
+    if(isDialogShow()) {
+      return dialogPromise;
+    }
+    publicInfo.status = 'show';
+    publicInfo.accountId = accountId;
+    publicInfo.password = password;
+    dialogPromise = $mdDialog.show(dialog);
+    return dialogPromise;
+  };
+  const close = () => {
+    return $mdDialog.hide()
+    .then(success => {
+      dialogPromise = null;
+      return;
+    }).catch(err => {
+      dialogPromise = null;
+      return;
+    });
+  };
+  const changePassword = () => {
+    if(!publicInfo.password) { return; }
+    publicInfo.status = 'loading';
+    userApi.changePassword(publicInfo.accountId, publicInfo.password)
+    .then(() => {
+      publicInfo.status = 'success';
+    })
+    .catch(() => {
+      publicInfo.status = 'error';
+    });
+  };
+  publicInfo.close = close;
+  publicInfo.changePassword = changePassword;
+  const dialog = {
+    templateUrl: '/public/views/user/changePassword.html',
+    escapeToClose: false,
+    locals: { bind: publicInfo },
+    bindToController: true,
+    controller: ['$scope', 'bind', ($scope, bind) => {
+      $scope.publicInfo = bind;
+      // $scope.changePassword = () => {
+      //   $mdDialog.cancel();
+      //   userApi.changePassword(accountId, $scope.account.password).then(() => {
+      //     getUserAccountInfo();
+      //   });
+      // };
+    }],
+    clickOutsideToClose: false,
+  };
+  // $mdDialog.show(dialog);
+  return {
+    show,
+    // close,
+    // error,
+    // success,
+  };
+}]);
