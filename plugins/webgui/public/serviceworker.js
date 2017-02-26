5195960e68206692ea94ed03a367e5e90c121f19
gyt-Macmini <igyteng@gmail.com>
2017-02-26 11:04:04 +0800

    bug fix

--- plugins/webgui/public/serviceworker.js
+++ plugins/webgui/public/serviceworker.js
@@ -1,6 +1,6 @@
 // importScripts('/libs/serviceworker-cache-polyfill.js');
 
-var ONLINE_CACHE_NAME = '2017-02-25 19:41:10';
+var ONLINE_CACHE_NAME = '2017-02-26 11:01:10';
 var onlineCacheUrl = [
   '/',
 
