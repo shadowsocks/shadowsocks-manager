da32bdd8c9be230e8ccab7f753dbba653ad14219
Guyuteng <igyteng@gmail.com>
2017-03-09 16:46:15 +0800

    bug fix

--- plugins/webgui/public/serviceworker.js
+++ plugins/webgui/public/serviceworker.js
@@ -1,6 +1,6 @@
 // importScripts('/libs/serviceworker-cache-polyfill.js');
 
-var ONLINE_CACHE_NAME = '2017-03-09 16:41:10';
+var ONLINE_CACHE_NAME = '2017-03-09 16:44:10';
 var onlineCacheUrl = [
   '/',
 
