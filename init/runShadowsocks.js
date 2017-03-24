162c67d811bc75424eec70be46d0c78ea87168f1
gyt-Macmini <igyteng@gmail.com>
2017-03-25 01:04:41 +0800

    bug fix

--- init/runShadowsocks.js
+++ init/runShadowsocks.js
@@ -8,30 +8,32 @@ const config = appRequire('services/config').all();
 const spawn = require('child_process').spawn;
 
 const run = async () => {
-  let type = config.runShadowsocks;
-  if(typeof type === 'boolean' && type) {
-    type = 'libev';
-  }
+  let runParams = config.runShadowsocks;
+  let type = 'libev';
   let method = 'aes-256-cfb';
-  if(!type) {
+  if(typeof runParams === 'boolean' && !runParams) {
     return;
   }
-  if(type.indexOf(':') >= 0) {
-    method = type.split(':')[1];
+  if(typeof runParams === 'boolean' && runParams) {
+    runParams = '';
+  }
+  if(runParams.indexOf(':') >= 0) {
+    method = runParams.split(':')[1];
   }
   let shadowsocks;
-  if(type === 'python') {
-    shadowsocks = spawn('ssserver', ['-m', method, '-p', '65535', '-k', 'qwerASDF395745725', config.shadowsocks.address]);
+  if(runParams.indexOf('python') >= 0) {
+    type = 'python';
+    shadowsocks = spawn('ssserver', ['-m', method, '-p', '65535', '-k', 'qwerASDF395745725', '--manager-address', config.shadowsocks.address]);
   } else {
     shadowsocks = spawn('ss-manager', [ '-m', method, '-u', '--manager-address', config.shadowsocks.address]);
   }
 
   shadowsocks.stdout.on('data', (data) => {
-    console.log(`stdout: ${data}`);
+    // console.log(`stdout: ${data}`);
   });
 
   shadowsocks.stderr.on('data', (data) => {
-    console.log(`stderr: ${data}`);
+    // console.log(`stderr: ${data}`);
   });
 
   shadowsocks.on('close', (code) => {
