e6321617d30194bcc8cdfc2515591558b38da825
gyteng <igyteng@gmail.com>
2017-04-17 11:29:52 +0800

    bug fix

--- plugins/webgui/public/serviceworker.js
+++ plugins/webgui/public/serviceworker.js
@@ -1,6 +1,6 @@
 // importScripts('/libs/serviceworker-cache-polyfill.js');
 
-var ONLINE_CACHE_NAME = '2017-04-16 02:30:27';
+var ONLINE_CACHE_NAME = '2017-04-17 11:30:27';
 var onlineCacheUrl = [
   '/',
 
