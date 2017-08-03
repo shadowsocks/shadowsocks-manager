37237760dcd734278f0e59eccfe0fce09933fe77
gyt-Macmini <igyteng@gmail.com>
2017-08-04 00:04:13 +0800

    translate

--- plugins/webgui/public/serviceworker.js
+++ plugins/webgui/public/serviceworker.js
@@ -1,6 +1,6 @@
 // importScripts('/libs/serviceworker-cache-polyfill.js');
 
-var ONLINE_CACHE_NAME = '2017-08-03 22:56:36';
+var ONLINE_CACHE_NAME = '2017-08-03 23:56:36';
 var onlineCacheUrl = [
   '/',
 
