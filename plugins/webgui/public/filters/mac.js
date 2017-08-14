b0a40f475c35ad7012ed50d8fcf3ec2a3e200a39
gyt-Macmini <igyteng@gmail.com>
2017-08-15 00:13:36 +0800

    pretty mac address

---
+++ plugins/webgui/public/filters/mac.js
@@ -0,0 +1,11 @@
+const app = angular.module('app');
+
+app.filter('mac', function() {
+  return function(mac) {
+    return mac.toUpperCase().split('').map((m, index, array) => {
+      if(index % 2 === 0) {
+        return m + array[index + 1];
+      }
+    }).filter(f => f).join('-');
+  };
+});
--- plugins/webgui/views/serviceworker.js
+++ plugins/webgui/views/serviceworker.js
@@ -1,6 +1,6 @@
 // importScripts('/libs/serviceworker-cache-polyfill.js');
 
-const ONLINE_CACHE_NAME = '2017-08-12 16:38:22' + ' <%= serviceWorkerTime%>';
+const ONLINE_CACHE_NAME = '2017-08-14 23:11:22' + ' <%= serviceWorkerTime%>';
 const isSWOpen = JSON.parse('<%= serviceWorker%>');
 
 const emptyCacheUrl = [];
