49e66b9685acfbb9e04f6638724166f81c8cd0fb
gyteng <igyteng@gmail.com>
2017-09-08 17:01:08 +0800

    fix #280

--- plugins/webgui/views/serviceworker.js
+++ plugins/webgui/views/serviceworker.js
@@ -1,6 +1,6 @@
 // importScripts('/libs/serviceworker-cache-polyfill.js');
 
-const ONLINE_CACHE_NAME = '2017-09-06 14:36:53' + ' <%= serviceWorkerTime%>';
+const ONLINE_CACHE_NAME = '2017-09-08 13:16:53' + ' <%= serviceWorkerTime%>';
 const isSWOpen = JSON.parse('<%= serviceWorker%>');
 
 const emptyCacheUrl = [];
