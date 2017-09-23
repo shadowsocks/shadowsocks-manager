9dd2361fc355617466fbdba36f57cfb851c5fe72
gyt-Macmini <igyteng@gmail.com>
2017-09-23 22:52:54 +0800

    bug fix

--- plugins/webgui/views/serviceworker.js
+++ plugins/webgui/views/serviceworker.js
@@ -1,6 +1,6 @@
 // importScripts('/libs/serviceworker-cache-polyfill.js');
 
-const ONLINE_CACHE_NAME = '2017-09-23 15:26:29' + ' <%= serviceWorkerTime%>';
+const ONLINE_CACHE_NAME = '2017-09-23 22:51:29' + ' <%= serviceWorkerTime%>';
 const isSWOpen = JSON.parse('<%= serviceWorker%>');
 
 const emptyCacheUrl = [];
