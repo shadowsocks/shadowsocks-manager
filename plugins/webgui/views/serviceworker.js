f976ada18db5ea9e61b830bb63eee4a668c8fcb2
Gyt <igyteng@gmail.com>
2018-03-30 07:37:10 +0800

    skin

--- plugins/webgui/views/serviceworker.js
+++ plugins/webgui/views/serviceworker.js
@@ -1,4 +1,4 @@
-const ONLINE_CACHE_NAME = '2018-03-29 23:51:42 <%= serviceWorkerTime%>';
+const ONLINE_CACHE_NAME = '2018-03-30 06:50:42 <%= serviceWorkerTime%>';
 const isSWOpen = JSON.parse('<%= serviceWorker%>');
 
 const emptyCacheUrl = [];
