47a71ba4b607382ab7c33d3b216cce62a0f37d51
GitLaboratory <sequentialsong@gmail.com>
2018-06-25 22:25:48 +0700

    添加俄语本地化

--- plugins/webgui/public/translate/index.js
+++ plugins/webgui/public/translate/index.js
@@ -2,7 +2,7 @@ const app = angular.module('app');
 app.config(['$translateProvider', $translateProvider => {
   $translateProvider.translations('en-US', require('./en-US.js'));
   $translateProvider.translations('zh-CN', require('./zh-CN.js'));
+  $translateProvider.translations('ru-RU', require('./ru-RU.js'));
   $translateProvider.preferredLanguage(navigator.language || 'zh-CN');
   $translateProvider.useSanitizeValueStrategy('escape');
 }]);
-
