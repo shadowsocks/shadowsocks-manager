073e7313ec7e7c73338b1578117f97eee345cbcc
粒粒橙 <i@llc.moe>
2018-10-26 00:42:00 -0700

    format code

--- init/checkConfig.js
+++ init/checkConfig.js
@@ -4,9 +4,6 @@ const program = require('commander');
 const version = appRequire('package').version;
 const log = appRequire('init/log');
 
-const log4js = require('log4js');
-const logger = log4js.getLogger('system');
-
 const ssmgrPath = path.resolve(os.homedir(), './.ssmgr/');
 
 program
@@ -14,29 +11,51 @@ program
   .option('-c, --config [file]', 'config file, default: ~/.ssmgr/default.yml')
   .option('-d, --db [file]', 'sqlite3 file, sample: ~/.ssmgr/db.sqlite')
   .option('-t, --type [type]', 'type, s for server side, m for manager side')
-  .option('-s, --shadowsocks [address]', 'ss-manager address, sample: 127.0.0.1:6001')
+  .option(
+    '-s, --shadowsocks [address]',
+    'ss-manager address, sample: 127.0.0.1:6001',
+  )
   .option('-m, --manager [address]', 'manager address, sample: 0.0.0.0:6002')
-  .option('-p, --password [password]', 'manager password, both server side and manager side must be equals')
-  .option('-r, --run [type]', 'run shadowsocks from child_process, sample: libev / libev:aes-256-cfb / python / python:aes-256-cfb')
+  .option(
+    '-p, --password [password]',
+    'manager password, both server side and manager side must be equals',
+  )
+  .option(
+    '-r, --run [type]',
+    'run shadowsocks from child_process, sample: libev / libev:aes-256-cfb / python / python:aes-256-cfb',
+  )
   .option('--plugin [name]', 'Enable SIP003 plugin. (Experimental)')
-  .option('--plugin-opts [options]', 'Set SIP003 plugin options. (Experimental)')
+  .option(
+    '--plugin-opts [options]',
+    'Set SIP003 plugin options. (Experimental)',
+  )
   .option('--debug', 'show debug message')
   .parse(process.argv);
 
-if(program.config) { global.configFile = program.config; }
+if (program.config) {
+  global.configFile = program.config;
+}
 
-if(!program.debug) {
+if (!program.debug) {
   log.setConsoleLevel('ERROR');
 }
 
 const config = appRequire('services/config');
 let logName = 'uname';
 
-if(program.type) {config.set('type', program.type);}
-if(program.shadowsocks) {config.set('shadowsocks.address', program.shadowsocks);}
-if(program.manager) {config.set('manager.address', program.manager);}
-if(program.password) {config.set('manager.password', program.password);}
-if(program.db) {
+if (program.type) {
+  config.set('type', program.type);
+}
+if (program.shadowsocks) {
+  config.set('shadowsocks.address', program.shadowsocks);
+}
+if (program.manager) {
+  config.set('manager.address', program.manager);
+}
+if (program.password) {
+  config.set('manager.password', program.password);
+}
+if (program.db) {
   config.set('db', program.db);
 }
 if (typeof config.get('db') === 'object') {
@@ -45,14 +64,14 @@ if (typeof config.get('db') === 'object') {
   const dbpath = config.get('db');
   logName = path.basename(dbpath).split('.')[0];
   if (dbpath[0] === '/' || dbpath[0] === '.' || dbpath[0] === '~') {
-	  config.set('db', path.resolve(dbpath));
+    config.set('db', path.resolve(dbpath));
   } else {
-	  config.set('db', path.resolve(ssmgrPath, dbpath));
+    config.set('db', path.resolve(ssmgrPath, dbpath));
   }
 }
 log.setFileAppenders(logName);
 
-if(program.run) {
+if (program.run) {
   config.set('runShadowsocks', program.run);
   if (program.plugin) {
     config.set('ssPlugin', program.plugin);
--- init/runShadowsocks.js
+++ init/runShadowsocks.js
@@ -12,13 +12,13 @@ const run = async () => {
   let plugin = config.ssPlugin;
   let pluginOpts = config.ssPluginOpts;
 
-  if(!runParams) {
+  if (!runParams) {
     return;
   }
-  if(runParams === true) {
+  if (runParams === true) {
     runParams = '';
   }
-  if(runParams.indexOf(':') >= 0) {
+  if (runParams.indexOf(':') >= 0) {
     method = runParams.split(':')[1];
   }
 
@@ -37,26 +37,45 @@ const run = async () => {
   }
 
   let shadowsocks;
-  if(runParams.indexOf('python') >= 0) {
+  if (runParams.indexOf('python') >= 0) {
     type = 'python';
-    const tempPassword = Math.random().toString(36).slice(2); //better random password generator
-    shadowsocks = spawn('ssserver', ['-m', method, '-p', '65535', '-k', tempPassword, '--manager-address', config.shadowsocks.address, ...moreArgs]);
+    const tempPassword = Math.random()
+      .toString(36)
+      .slice(2); //better random password generator
+    shadowsocks = spawn('ssserver', [
+      '-m',
+      method,
+      '-p',
+      '65535',
+      '-k',
+      tempPassword,
+      '--manager-address',
+      config.shadowsocks.address,
+      ...moreArgs,
+    ]);
   } else {
-    shadowsocks = spawn('ss-manager', [ '-m', method, '-u', '--manager-address', config.shadowsocks.address, ...moreArgs]);
+    shadowsocks = spawn('ss-manager', [
+      '-m',
+      method,
+      '-u',
+      '--manager-address',
+      config.shadowsocks.address,
+      ...moreArgs,
+    ]);
   }
 
-  shadowsocks.stdout.on('data', (data) => {
+  shadowsocks.stdout.on('data', data => {
     // console.log(`stdout: ${data}`);
   });
 
-  shadowsocks.stderr.on('data', (data) => {
+  shadowsocks.stderr.on('data', data => {
     // console.error(`stderr: ${data}`);
   });
 
-  shadowsocks.on('close', (code) => {
+  shadowsocks.on('close', code => {
     console.log(`child process exited with code ${code}`);
   });
-  logger.info(`Run shadowsocks (${ type === 'python' ? 'python' : 'libev'})`);
+  logger.info(`Run shadowsocks (${type === 'python' ? 'python' : 'libev'})`);
   return;
 };
 
