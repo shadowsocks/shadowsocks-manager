1b5ba69d2a640a648937b49f11b40fdaae945d4f
粒粒橙 <i@llc.moe>
2018-10-27 21:06:39 -0700

    add ss-server args to -r mode

--- init/runShadowsocks.js
+++ init/runShadowsocks.js
@@ -9,36 +9,80 @@ const run = async () => {
   let runParams = config.runShadowsocks;
   let type = 'libev';
   let method = 'aes-256-cfb';
-  if(!runParams) {
+  let plugin = config.ssPlugin;
+  let pluginOpts = config.ssPluginOpts;
+
+  if (!runParams) {
     return;
   }
-  if(typeof runParams === 'boolean' && runParams) {
+  if (runParams === true) {
     runParams = '';
   }
-  if(runParams.indexOf(':') >= 0) {
+  if (runParams.indexOf(':') >= 0) {
     method = runParams.split(':')[1];
   }
-  let shadowsocks;
-  if(runParams.indexOf('python') >= 0) {
-    type = 'python';
-    const tempPassword = 'qwerASDF' + Math.random().toString().substr(2, 8);
-    shadowsocks = spawn('ssserver', ['-m', method, '-p', '65535', '-k', tempPassword, '--manager-address', config.shadowsocks.address]);
-  } else {
-    shadowsocks = spawn('ss-manager', [ '-m', method, '-u', '--manager-address', config.shadowsocks.address]);
+
+  let moreArgs = [];
+  if (plugin) {
+    moreArgs.push('--plugin');
+    if (plugin !== true) {
+      moreArgs.push(plugin);
+    }
+    if (pluginOpts) {
+      moreArgs.push('--plugin-opts');
+      if (pluginOpts !== true) {
+        moreArgs.push(pluginOpts);
+      }
+    }
   }
 
-  shadowsocks.stdout.on('data', (data) => {
+  if (config.ssFastOpen) {
+    moreArgs.push('--fast-open');
+  }
+  if (config.ssNoDelay) {
+    moreArgs.push('--no-delay');
+  }
+
+  let shadowsocks;
+  if (runParams.indexOf('python') >= 0) {
+    type = 'python';
+    const tempPassword = Math.random()
+      .toString(36)
+      .slice(2); //better random password generator
+    shadowsocks = spawn('ssserver', [
+      '-m',
+      method,
+      '-p',
+      '65535',
+      '-k',
+      tempPassword,
+      '--manager-address',
+      config.shadowsocks.address,
+      ...moreArgs,
+    ]);
+  } else {
+    shadowsocks = spawn('ss-manager', [
+      '-m',
+      method,
+      '-u',
+      '--manager-address',
+      config.shadowsocks.address,
+      ...moreArgs,
+    ]);
+  }
+
+  shadowsocks.stdout.on('data', data => {
     // console.log(`stdout: ${data}`);
   });
 
-  shadowsocks.stderr.on('data', (data) => {
+  shadowsocks.stderr.on('data', data => {
     // console.error(`stderr: ${data}`);
   });
 
-  shadowsocks.on('close', (code) => {
+  shadowsocks.on('close', code => {
     console.log(`child process exited with code ${code}`);
   });
-  logger.info(`Run shadowsocks (${ type === 'python' ? 'python' : 'libev'})`);
+  logger.info(`Run shadowsocks (${type === 'python' ? 'python' : 'libev'})`);
   return;
 };
 
