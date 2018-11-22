7d69a99c61fb3ed14d536f90a3ff6df21ea007db
Gyt <igyteng@gmail.com>
2018-11-23 07:54:33 +0800

    set default group

--- plugins/webgui/public/controllers/adminSetting.js
+++ plugins/webgui/public/controllers/adminSetting.js
@@ -1,7 +1,7 @@
 const app = angular.module('app');
 
-app.controller('AdminSettingsController', ['$scope', '$http', '$timeout', '$state',
-  ($scope, $http, $timeout, $state) => {
+app.controller('AdminSettingsController', ['$scope', '$state',
+  ($scope, $state) => {
     $scope.setTitle('设置');
     $scope.toSetting = path => { $state.go(path); };
     if($scope.id === 1) {
@@ -116,6 +116,12 @@ app.controller('AdminSettingsController', ['$scope', '$http', '$timeout', '$stat
       $scope.$watch('accountServerObj', () => {
         $scope.saveSetting();
       }, true);
+      return $http.get('/api/admin/group');
+    }).then(success => {
+      $scope.groups = success.data;
+      if(!($scope.accountData.defaultGroup >= 0)) {
+        $scope.accountData.defaultGroup = 0;
+      }
     });
   }
 ]).controller('AdminBaseSettingController', ['$scope', '$http', '$timeout', '$state', '$q',
