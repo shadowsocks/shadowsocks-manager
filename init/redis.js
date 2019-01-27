07c691894ad9aae060b79f21e8fc49877937a0eb
Gyt <igyteng@gmail.com>
2019-01-27 21:20:21 +0800

    support redis local domain socket path

--- init/redis.js
+++ init/redis.js
@@ -1,9 +1,4 @@
 const config = appRequire('services/config').get('redis');
 const Redis = require('ioredis');
-const redis = new Redis({
-  host: config.host,
-  port: config.port || 6379,
-  password: config.password,
-  db: config.db || 0,
-});
+const redis = new Redis(config);
 exports.redis = redis;
