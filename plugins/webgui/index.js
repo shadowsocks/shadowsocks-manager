9513563d900b0a369fe48e4f77918c5a70bb7ed7
Gyt-OMEN <igyteng@gmail.com>
2019-12-12 23:26:47 +0800

    bug fix

--- plugins/webgui/index.js
+++ plugins/webgui/index.js
@@ -54,6 +54,7 @@ app.set('view engine', 'html');
 app.set('views', path.resolve('./plugins/webgui/views'));
 
 app.use('/libs', express.static(path.resolve('./plugins/webgui/libs')));
+app.use('/libs', express.static(path.resolve(os.homedir(), './.ssmgr/libs')));
 app.use('/public', express.static(path.resolve('./plugins/webgui/public')));
 app.use('/public/views/skin', express.static(path.resolve(os.homedir(), './.ssmgr/skin')));
 
