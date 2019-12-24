d348de55f4d88245c06e12a5cff3dac7355615bf
Gyt-OMEN <igyteng@gmail.com>
2019-12-24 22:23:50 +0800

    bug fix

--- plugins/webgui/views/serviceworker.js
+++ plugins/webgui/views/serviceworker.js
@@ -1,4 +1,4 @@
-const ONLINE_CACHE_NAME = '2019-12-22 20:11:28 <%= serviceWorkerTime%>';
+const ONLINE_CACHE_NAME = '2019-12-24 22:17:08 <%= serviceWorkerTime%>';
 const isSWOpen = JSON.parse('<%= serviceWorker%>');
 
 const emptyCacheUrl = [];
